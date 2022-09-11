16ccf6c031511317ef6809f40fe13b3f7116f74f
Seyipaul089 <olayemipaul089@gmail.com>
2022-09-11 15:01:10 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming_is_like_building_a_multilingual_puzzle");
+	 char test_str[] = "\"Programming is like building a multilingual puzzle
+
+	 puts(test_str);		 
 	 return (0);
 
 }
