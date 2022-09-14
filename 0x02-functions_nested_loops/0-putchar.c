7f37a09c8410fb668cd93b1bc8a333baa192c0ac
Seyipaul089 <olayemipaul089@gmail.com>
2022-09-14 04:35:52 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ *
+ * Returnn: Always 0 (success)
+ *\
+
+int main(void)
+
+{
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('c');
+
+	_putchar('t');
+
+	_putchar("\n");
+
+	return (0);
+}
