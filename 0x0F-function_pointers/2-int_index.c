f0eb20e7aad1ff460cdaa78bab545a21de0fd1bd
Seyipaul089 <Olayemipaul089@gmail.com>
2022-10-14 03:56:27 -0700

    func pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - earches for an integer
+ * @array: array to search in
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ *
+ * Return: index of the first element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
+	}
+
+	return (-1);
+}
