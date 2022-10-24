6d56b0e89f110d77bf1531fff7743d2630e6d345
Seyipaul089 <Olayemipaul089@gmail.com>
2022-10-24 02:06:44 -0700

    linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,15 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t count;
+	int i = 0;
 
-	count = 0;
+	if (h == NULL)
+		return (0);
 	while (h != NULL)
 	{
+		if (h->str != NULL)
+			i++;
 		h = h->next;
-		count++;
 	}
-	return (count);
-
+	return (i);
+}
